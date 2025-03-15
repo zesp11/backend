1b9072bcf8da943a04b97fedbb46d7e061ce2ccd
K4-mil <166294210+K4-mil@users.noreply.github.com>
2025-03-15 21:13:12 +0100

    test

--- goadventure/src/main/java/adventure/go/goadventure/image/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package adventure.go.goadventure.image;
-
-import adventure.go.goadventure.image.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
